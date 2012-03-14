c2e3e505656da783972289d3f4c0017b8d60a0dc
Benny Jonsson <jonsson.benny@gmail.com>
2012-03-14 22:12:31 +0100

    init

---
+++ Sam/Food.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sam
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private int i = 0;
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public int I() { i++; return i; }
+        public void Reset() {i = 0;}
+
+    }
+
+    public abstract class Food {
+        public string Eat() {
+            Singleton singleton = Singleton.Instance;
+            if (singleton.I() > 5)
+            {
+                singleton.Reset();
+                throw new Exception("Puke");
+            }
+            return "Yumm"; }
+    }
+}
---
+++ SamTestProject/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sam;
+
+namespace SamTestProject
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+}
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void EatCookie()
+        {
+            Assert.AreEqual("Yumm", new Cookie().Eat());
+        }
+
+        [TestMethod]
+        public void EatDirt()
+        {
+            Assert.AreEqual("Yumm", new Dirt().Eat());
+        }
+
+        [TestMethod]
+        public void EatALot()
+        {
+            try
+            {
+                new Dirt().Eat();
+                new Cookie().Eat();
+                new Apple().Eat();
+                new Dirt().Eat();
+                new Dirt().Eat();
+                new Cookie().Eat();
+                new Apple().Eat();
+                new Dirt().Eat();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
